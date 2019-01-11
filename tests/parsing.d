297fc0859600e780aa238f8f3abf413bce42909a
unleashy <lakeavenger@gmail.com>
2019-01-11 00:48:40 -0200

    add whitespace tests

--- tests/parsing.d
+++ tests/parsing.d
@@ -9,17 +9,22 @@ import tddjson;
 void testEmptyStringFails()
 {
     parseJSON("").shouldThrow!(JSONException);
+    parseJSON(" \t\r\n").shouldThrow!(JSONException);
 }
 
 void testParseBoolean()
 {
-    parseJSON("true").should  == true;
-    parseJSON("false").should == false;
+    parseJSON("true").should    == true;
+    parseJSON("false").should   == false;
+    parseJSON(" \t\r\ntrue \t\r\n").should   == true;
+    parseJSON(" \t\r\nfalse \t\r\n").should  == false;
 }
 
 void testParseNull()
 {
-    parseJSON("null").should == null;
+    parseJSON("null").should     == null;
+    parseJSON(" \t\r\nnull \t\r\n").should   == null;
+    parseJSON(" \t\r\nnull \t\r\n").should == null;
 }
 
 void testParseNumber()
