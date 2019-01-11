6fc5aac608b7257cf8dafa22a367656ecae21b7f
unleashy <lakeavenger@gmail.com>
2019-01-11 00:39:52 -0200

    add basic implementation of number parsing

--- source/tddjson/parsing.d
+++ source/tddjson/parsing.d
@@ -5,13 +5,60 @@ import tddjson.value;
 
 @safe:
 
-JSONValue parseJSON(in string s)
+JSONValue parseJSON(string str)
 {
-    switch (s) {
-        case "true":  return JSONValue(true);
-        case "false": return JSONValue(false);
-        case "null":  return JSONValue(null);
-        case "0":     return JSONValue(0);
-        default:      throw new JSONException("parsing error");
+    if (str == "true") {
+        return JSONValue(true);
+    } else if (str == "false") {
+        return JSONValue(false);
+    } else if (str == "null") {
+        return JSONValue(null);
+    } else if (auto parsedNum = parseNumber(str)) {
+        return JSONValue(parsedNum.value);
     }
+
+    throw new JSONException("parsing error");
+}
+
+private struct ParseResult(T)
+{
+    T value;
+    bool successful;
+
+    bool opCast(T : bool)() const
+    {
+        return successful;
+    }
+}
+
+private ParseResult!T parseResultOk(T)(T value)
+{
+    return ParseResult!T(value, true);
+}
+
+private ParseResult!T parseResultFail(T)()
+{
+    return ParseResult!T(T.init, false);
+}
+
+private ParseResult!long parseNumber(ref string str)
+{
+    import std.range.primitives : empty, front, popFront;
+
+    if (str.empty) return parseResultFail!(long);
+
+    long value = 0;
+
+    for (; !str.empty; str.popFront()) {
+        immutable c = str.front;
+        switch (c) {
+            case '0': .. case '9':
+                value = value * 10 + (c - '0');
+                break;
+
+            default: break;
+        }
+    }
+
+    return parseResultOk(value);
 }
