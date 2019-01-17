4128aa5f315deeb323dd9ff63d144a8971facfc4
unleashy <lakeavenger@gmail.com>
2019-01-17 02:36:01 -0200

    fix formatting

--- tests/parsing.d
+++ tests/parsing.d
@@ -17,16 +17,16 @@ void testMalformedFails()
 
 void testParseBoolean()
 {
-    parseJSON("true").should    == true;
-    parseJSON("false").should   == false;
-    parseJSON(" \t\r\ntrue \t\r\n").should   == true;
-    parseJSON(" \t\r\nfalse \t\r\n").should  == false;
+    parseJSON("true").should  == true;
+    parseJSON("false").should == false;
+    parseJSON(" \t\r\ntrue \t\r\n").should  == true;
+    parseJSON(" \t\r\nfalse \t\r\n").should == false;
 }
 
 void testParseNull()
 {
-    parseJSON("null").should     == null;
-    parseJSON(" \t\r\nnull \t\r\n").should   == null;
+    parseJSON("null").should == null;
+    parseJSON(" \t\r\nnull \t\r\n").should == null;
     parseJSON(" \t\r\nnull \t\r\n").should == null;
 }
 
